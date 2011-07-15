2b581a64d27d9861fa54fa0a55b52e1caf7937cc
Arcath <adam@arcath.net>
2011-07-15 16:00:20 +0100

    Add denied_groups option, closes #6

--- lib/adauth/config.rb
+++ lib/adauth/config.rb
@@ -1,10 +1,11 @@
 module Adauth
     class Config
-        attr_accessor :domain, :port, :base, :server, :allowed_groups
+        attr_accessor :domain, :port, :base, :server, :allowed_groups, :denied_groups
         
         def initialize
            @port = 389
            @allowed_groups = []
+           @denied_groups = []
         end
     end
 end
