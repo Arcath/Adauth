e0f17dde356367050f6197cf48fa6d69cf84f536
Adam Laycock <adam@arcath.net>
2013-08-30 09:51:07 +0100

    make changes to travis settings for ldap testing

---
+++ travis/before_script.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+mkdir log
+
+password = "secret"
+crypted_password = `slappasswd -s $password`
+cat <<EOF | sudo ldapmodify -Y EXTERNAL -H ldapi:///
+version: 1
+dn: olcDatabase={1}hdb,cn=config
+changetype: modify
+replace: olcRootPW
+olcRootPW: ${crypted_password}
+-
+EOF
+
+sudo ldapmodify -Y EXTERNAL -H ldapi:/// -f travis/ldif/phonetic-attribute-options.ldif
+
+base="dc=`echo get slapd/domain | sudo debconf-communicate slapd | sed -e 's/^0 //' | sed -e 's/^\.//; s/\./,dc=/g'`"
+
+cat <<EOF > spec/test_data.yml
+domain:
+  domain: ed-itsolutions.com
+  port: 389
+  base: ${base}
+  server: 127.0.0.1
+  query_user: admin
+  query_password: ${password}
+  breakable_user: foo
+  breakable_password: bar
+  testable_ou: Foo
+EOF
