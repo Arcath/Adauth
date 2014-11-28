df08e7b963799c0dcde2457bbe006c18ce7a1e0a
Miroslav Kyurchev <mkyurchev@gmail.com>
2014-11-28 18:46:45 +0300

    Allow to load objects with empty fields

--- lib/adauth/ad_object.rb
+++ lib/adauth/ad_object.rb
@@ -2,13 +2,13 @@ module Adauth
     # Container for Objects which inherit from Adauth::AdObject
     module AdObjects
     end
-  
+
     # Add a field to the specified model
     def self.add_field(object, adauth_method, ldap_method)
         Adauth.logger.info(object.inspect) { "Adding field \"#{ldap_method}\"" }
         object::Fields[adauth_method] = ldap_method
     end
-    
+
     # Active Directory Interface Object
     #
     # Objects inherit from this class.
@@ -16,7 +16,7 @@ module Adauth
     # Provides all the common functions for Active Directory.
     class AdObject
         include Expects
-        
+
         def self.method_missing(method, *args)
           return super unless method =~ /^find_by_/
           method_field = method.to_s.split("_").last
@@ -24,13 +24,13 @@ module Adauth
           return super unless field
           self.where(field, args.first)
         end
-        
+
         def method_missing(method, *args)
           field = self.class::Fields[method]
           return handle_field(field) if field
           return super
         end
-        
+
         def self.reverse_field(search)
           hash = {}
           self::Fields.each do |k, v|
@@ -38,13 +38,13 @@ module Adauth
           end
           return hash[search]
         end
-        
+
         # Returns all objects which have the ObjectClass of the inherited class
         def self.all
             Adauth.logger.info(self.class.inspect) { "Searching for all objects matching filter \"#{self::ObjectFilter}\"" }
             Adauth::SearchResults.new(self.filter(self::ObjectFilter))
         end
-        
+
         # Returns all the objects which match the supplied query
         #
         # Uses ObjectFilter to restrict to the current object
@@ -53,7 +53,7 @@ module Adauth
             Adauth.logger.info(self.class.inspect) { "Searching for all \"#{self::ObjectFilter}\" where #{field} = #{value}" }
             Adauth::SearchResults.new(filter(add_object_filter(search_filter)))
         end
-        
+
         # Returns all LDAP objects that match the given filter
         #
         # Use with add_object_filter to make sure that you only get objects that match the object you are querying though
@@ -70,31 +70,31 @@ module Adauth
 
           results
         end
-        
+
         # Adds the object filter to the passed filter
         def self.add_object_filter(filter)
           filter & self::ObjectFilter
         end
-        
-        # Creates a new instance of the object and sets @ldap_object to the passed Net::LDAP entity        
+
+        # Creates a new instance of the object and sets @ldap_object to the passed Net::LDAP entity
         def initialize(ldap_object)
             expects ldap_object, Net::LDAP::Entry
             @ldap_object = ldap_object
         end
-        
-        # Allows direct access to @ldap_object 
+
+        # Allows direct access to @ldap_object
         def ldap_object
             @ldap_object
         end
-        
+
         # Handle the output for the given field
         def handle_field(field)
           case field
             when Symbol then return return_symbol_value(field)
-            when Array then return  @ldap_object.send(field.first).collect(&field.last)
+            when Array then return  @ldap_object.try(field.first).try(:collect, &field.last)
           end
         end
-        
+
         # Returns all the groups the object is a member of
         def groups
             unless @groups
@@ -102,7 +102,7 @@ module Adauth
             end
             @groups
         end
-        
+
         # The same as cn_groups, but with the parent groups included
         def cn_groups_nested
           @cn_groups_nested = cn_groups
@@ -117,7 +117,7 @@ module Adauth
           end
           return @cn_groups_nested
         end
-        
+
         # Returns all the ous the object is in
         def ous
             unless @ous
@@ -128,7 +128,7 @@ module Adauth
             end
             @ous
         end
-        
+
         # CSV Version of the ous list (can't be pulled over from AD)
         def dn_ous
             unless @dn_ous
@@ -139,7 +139,7 @@ module Adauth
             end
             @dn_ous
         end
-        
+
         # Runs a modify action on the current object, takes an aray of operations
         def modify(operations)
           Adauth.logger.info(self.class.inspect) { "Attempting modify operation" }
@@ -148,7 +148,7 @@ module Adauth
             raise 'Modify Operation Failed (see log for details)'
           end
         end
-        
+
         # Returns an array of member objects for this object
         def members
             unless @members
@@ -161,7 +161,7 @@ module Adauth
             end
             @members
         end
-        
+
         # Checks to see if the object is a member of a given parent (though DN)
         def is_a_member?(parent)
           my_split_dn = @ldap_object.dn.split(",")
@@ -171,14 +171,14 @@ module Adauth
           end
           return false
         end
-        
+
         # Delete the object
         def delete
           Adauth.connection.delete(dn: @ldap_object.dn)
         end
-        
+
         private
-        
+
         def convert_to_objects(array)
             out = []
             array.each do |entity|
@@ -186,18 +186,19 @@ module Adauth
             end
             out
         end
-        
+
         def convert_to_object(entity)
             user = Adauth::AdObjects::User.where('sAMAccountName', entity).first
             group = Adauth::AdObjects::Group.where('sAMAccountName', entity).first
             (user || group)
         end
-        
+
         def return_symbol_value(field)
-          value = @ldap_object.send(field)
+          value = @ldap_object.try(field)
+
           case value
-            when String then return value
             when Net::BER::BerIdentifiedArray then return value.first
+            else return value
           end
         end
     end
